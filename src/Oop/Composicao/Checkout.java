fda55653587e6c7be5a96aec7a1b02f1dc7a8916
Diego-World <contato.diegogouveia@gmail.com>
2023-09-20 07:32:50 -0300

    Implementação de method overloading

--- src/Oop/Composicao/Checkout.java
+++ src/Oop/Composicao/Checkout.java
@@ -4,9 +4,9 @@ public class Checkout {
     public static void main(String[] args) {
         Compra c1 = new Compra();
         c1.nomeCliente = "Diego";
-        c1.adicionarItem(new Item("Iphone 15 Pro Max",2,9000.00));
-        c1.adicionarItem(new Item("Camera Profissional Sony",1,7000.00));
-        c1.adicionarItem(new Item("Tripé",3,2300.00));
+        c1.adicionarItem("Iphone 15 Pro Max",2,9000.00);
+        c1.adicionarItem("Camera Profissional Sony",1,7000.00);
+        c1.adicionarItem("Tripé",3,2300.00);
 
         System.out.println(c1);
         System.out.println(c1.itens.size());
--- src/Oop/Composicao/Compra.java
+++ src/Oop/Composicao/Compra.java
@@ -6,11 +6,13 @@ public class Compra {
     String nomeCliente;
     ArrayList<Item> itens = new ArrayList<>();
 
-
-
     void adicionarItem(Item item){
         this.itens.add(item);
         item.compra = this;
+    }
+
+    void adicionarItem(String nome, int quantidade,double preco){
+        this.adicionarItem(new Item(nome, quantidade, preco));
     }
 
     double getValorTotal(){
