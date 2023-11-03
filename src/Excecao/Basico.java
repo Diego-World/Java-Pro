08083b1b595c98330da954789c622c2bf19de748
Diego-World <contato.diegogouveia@gmail.com>
2023-11-03 12:34:44 -0300

    Tratamento de errors

---
+++ src/Excecao/Basico.java
@@ -0,0 +1,29 @@
+package Excecao;
+
+import streams.Aluno;
+
+public class Basico {
+    public static void main(String[] args) {
+
+        Aluno a1 = null;
+        try {
+            imprimirNomeDoAluno(a1);
+        } catch (Exception e){
+            System.out.println("Ocorreu um erro no momento de imprimir o nome do Aluno");
+        }
+
+        try {
+            System.out.println(7/0);
+        }catch (ArithmeticException e){
+            System.out.println("OCorreu o erro: " + e.getMessage());
+        }
+
+        System.out.println("fim :)");
+
+    }
+
+    public static void imprimirNomeDoAluno(Aluno aluno){
+        System.out.println(aluno.nome);
+    }
+
+}
